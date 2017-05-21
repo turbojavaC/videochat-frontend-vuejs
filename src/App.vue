1245557e15bc6825bd293ba35bc6512f1f790e02
Oleg Korol <oleg@olegkorol.de>
2017-05-21 22:59:12 +0200

    Barebones of main page
    Took 5 minutes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,67 @@
 <template>
     <div id="app">
-        <greeting v-if="!authenticated"></greeting>
-        <authentication></authentication>
+        <div id="greeting_block" v-if="!authenticated">
+            <greeting></greeting>
+            <authentication></authentication>
+        </div>
+
+        <v-card v-if="authenticated" height="100vh">
+            <v-card-row class="blue white--text" height="10vh">
+                <v-menu bottom right>
+                    <v-btn icon="icon" slot="activator" dark>
+                        <v-icon>more_vert</v-icon>
+                    </v-btn>
+                    <v-list>
+                        <v-list-item v-for="item in items" :key="item">
+                            <v-list-tile>
+                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                            </v-list-tile>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+
+                <v-card-title>Hello!</v-card-title>
+
+                <v-menu bottom right style="width:120px">
+                    <v-btn icon="icon" slot="activator" dark style="width:100px">
+                        <v-icon>account_circle</v-icon>
+                        / ACCOUNT
+                    </v-btn>
+                    <v-list>
+                        <v-list-item v-for="item in items" :key="item">
+                            <v-list-tile>
+                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                            </v-list-tile>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </v-card-row>
+            <v-card-text style="background-color:aliceblue; height:90vh">
+                // CONTENT GOES HERE
+            </v-card-text>
+        </v-card>
+
     </div>
 </template>
 
 <script>
-    import Greeting from './components/Greeting.vue'
+	import Greeting from './components/Greeting.vue'
 	import { bus } from './main.js'
 
 	export default {
 		name: 'app',
-        components: {
-		    'greeting': Greeting
-        },
+		components: {
+			'greeting': Greeting
+		},
 		data () {
 			return {
-                authenticated: false
+				authenticated: false,
+                items: [
+                    {title: 'First'},
+                    {title: 'Second'},
+					{title: 'Third'},
+					{title: 'Fourth'}
+                ]
 			}
 		},
 		methods: {
@@ -30,16 +75,12 @@
 		created () {
 			this.checkAuthStatus();
 			bus.$on('authStatus', this.updateAuthStatus)
-		},
-		updated () {
-			this.checkAuthStatus();
-			bus.$on('authStatus', this.updateAuthStatus)
 		}
 	}
 </script>
 
 <style scoped>
-    #app {
+    #greeting_block {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
@@ -47,7 +88,5 @@
         color: #2c3e50;
         margin-top: 60px;
     }
-    .hidden {
-        display: none
-    }
+
 </style>
